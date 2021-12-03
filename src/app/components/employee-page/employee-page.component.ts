7c1184e2d0f6e7144bb9c1758b80c3adae0273c9
ferdiaz93 <fernandodiaz1209@gmail.com>
2021-12-03 19:20:06 -0300

    fix css to sass

--- src/app/components/employee-page/employee-page.component.ts
+++ src/app/components/employee-page/employee-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-page',
   templateUrl: './employee-page.component.html',
-  styleUrls: ['./employee-page.component.css']
+  styleUrls: ['./employee-page.component.scss']
 })
 export class EmployeePageComponent implements OnInit {
 
